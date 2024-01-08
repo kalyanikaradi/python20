9299c3ba73684e137ca95730db94ce92adbf476a
kalyanikaradi <154000642+kalyanikaradi@users.noreply.github.com>
2024-01-08 21:07:39 +0530

    Add files via upload

---
+++ 8 jan.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math
+x = math.sqrt(64)
+print(x)
+
+
+# In[2]:
+
+
+import math
+x = math.ceil(1.6)#ceil check onward
+y = math.floor(1.4)#floor check backword num
+
+print(x)
+print(y)
+
+
+# In[3]:
+
+
+import datetime
+x = datetime.datetime.now()
+print(x)
+
+
+# In[4]:
+
+
+import datetime
+x = datetime.datetime.now()
+print(x.year)
+print(x.strftime("%a"))
+
+
+# In[5]:
+
+
+import datetime
+x=datetime.datetime(2020,5,17)
+print(x)
+
+
+# In[6]:
+
+
+import datetime
+x = datetime.datetime(2018,6,1)
+print(x.strftime("%b"))
+
+
+# In[7]:
+
+
+import datetime
+x = datetime.datetime.now()
+print(x.strftime("%b"))
+
+
+# In[8]:
+
+
+import datetime
+x = datetime.datetime.now()
+print(x.strftime("%A"))
+
+
+# In[9]:
+
+
+import os
+os.getcwd()
+
+
+# In[10]:
+
+
+os.chdir
+
+
+# In[11]:
+
+
+pwd
+
+
+# In[ ]:
+
+
+
+
